af1ef9042bfe603f43be6879db7169751335c596
alfamizar <52598586+alfamizar@users.noreply.github.com>
2020-09-08 09:22:54 +0300

    Update ProcessesAndThreadsExamples.java

--- ProcessesAndThreadsExamples.java
+++ ProcessesAndThreadsExamples.java
@@ -37,3 +37,44 @@ private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         mImageView.setImageBitmap(result);
     }
 }
+
+
+// Обычно используется фоновый поток для выполнения сетевых операций или длительных задач, а затем обновлять пользовательский интерфейс при необходимости.
+
+// Это создает проблему, так как только основной поток может обновлять пользовательский интерфейс.
+
+// Решение заключается в использовании runOnUiThread() , поскольку он позволяет вам инициировать выполнение кода в потоке пользовательского интерфейса из фонового потока.
+
+// В этом простом примере поток запускается при создании Activity, выполняется до тех пор, пока магическое число 42 будет произвольно сгенерировано, а затем использует метод runOnUiThread() для обновления пользовательского интерфейса после выполнения этого условия.
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.my_text_view);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    //do stuff....
+                    Random r = new Random();
+                    if (r.nextInt(100) == 42) {
+                       break;
+                    }
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTextView.setText("Ready Player One");
+                    }
+                });
+            }
+        }).start();
+    }
+}
